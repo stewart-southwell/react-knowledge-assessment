3b2cd848ba2518bf9c250d7a0094e92e0f60eb49
Stewart <Stewart@dlxdskadmins-macbook-pro.local>
2020-09-29 19:16:28 -0400

    Tweaks

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { nanoid } from "nanoid";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
-//import logo from './logo.svg';
 import "./App.css";
 import { usePrevious } from "./hooks/usePrevious";
 
@@ -103,24 +102,5 @@ function App(props) {
     </div>
   );
 }
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
